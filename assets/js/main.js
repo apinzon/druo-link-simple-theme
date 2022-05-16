40532fd0382532b3e791811718b8221042049934
Maksym Tsypliakov <maksym.tsypliakov@gmail.com>
2022-05-16 16:43:34 +0200

    Use acf code field

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,6 @@
 (function($){$(function () {
 
     $(document).ready(function($) {
-        // Init code mirror editor.
-        $('.css-editor').each(function() {
-            let field = $(this).find('textarea');
-            let editor = wp.codeEditor.initialize( field, druo_simple.cm_settings );
-            editor.codemirror.setCursor(1);
-            $(document).on('keyup', '.CodeMirror-code', function(){
-                field.html(editor.codemirror.getValue());
-                field.trigger('change');
-            });
-        });
 
     });
 
